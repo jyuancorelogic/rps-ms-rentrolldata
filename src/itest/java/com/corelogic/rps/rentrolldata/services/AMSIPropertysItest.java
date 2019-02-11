56585466a67458e46f62fa5a4c93b25840aad234
Baskaran <675321@cognizant.com>
2019-02-11 19:58:26 +0530

    build issues

--- src/itest/java/com/corelogic/rps/rentrolldata/services/AMSIPropertysItest.java
+++ src/itest/java/com/corelogic/rps/rentrolldata/services/AMSIPropertysItest.java
@@ -31,7 +31,7 @@ public class AMSIPropertysItest {
     @Test
     public void getPropertyListAMSItest() throws MalformedURLException{
      String result="";
-		result = amsiservice.getPropertyListAMSI(null);
+		result = amsiservice.getPropertyListAMSI();
 	
         Assertions.assertThat(result.length()).isGreaterThan(0);
     }
