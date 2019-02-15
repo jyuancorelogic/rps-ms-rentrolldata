be0a85f03cbcb460e99b7681d323d05a2c7834ed
Baskaran <675321@cognizant.com>
2019-02-15 13:11:47 +0530

    Critical issues in sonar

--- src/main/java/com/corelogic/rps/rentrolldata/amsi/service/AmsiService.java
+++ src/main/java/com/corelogic/rps/rentrolldata/amsi/service/AmsiService.java
@@ -5,11 +5,9 @@ import com.corelogic.rps.rentrolldata.amsi.util.AMSIUtil;
 import com.corelogic.rps.rentrolldata.amsi.generated.LeasingSoap;
 import lombok.extern.log4j.Log4j2;
 
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Service;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * Created by 675321 on 2/6/2019.
@@ -22,69 +20,49 @@ public class AmsiService {
 
 
 
-    public String getPropertyListAMSI(String url, String username, String password,String portfolio ) {
+	public String getPropertyListAMSI(String username, String password,String portfolio ) {
+		String result = "";    
+
+		LeasingSoap port = AMSIUtil.getProxy();
+		result = port.getPropertyList(username,password, portfolio, "");
+		if (log.isInfoEnabled()) {
+			log.info("getPropertyListAMSI", result);
+		}
+
+		return result;
+	}
+
+
+	public String getPropertyResidents( String username, String password,String portfolio, String propertyId) {
+		String result = "";
+
+		LeasingSoap port = AMSIUtil.getProxy();
+		String proprequest = AMSIUtil.processHR(propertyId);
+		result = port.getPropertyResidents(username,password, portfolio, proprequest);
+		if (log.isInfoEnabled()) {
+			log.info("getPropertyResidents", result);
+		}
+
+		return result;
+
+	}
+
+    public String getPropertyUnits( String username, String password,String portfolio , String propertyId) {
         String result = "";
-        try {
-            log.info(url);
-            URL urlAddress = new URL(url);
-            LeasingSoap port = AMSIUtil.getProxy(urlAddress);
-            result = port.getPropertyList(username,password, portfolio, "");
-                      if (log.isInfoEnabled()) {
-                log.info("getPropertyListAMSI", result);
-            }
-        } catch (MalformedURLException e) {
-            if (log.isInfoEnabled()) {
-                log.info("MalformedURLException in getPropertyListAMSI", e);
-            }
-
-        }
-        return result;
-    }
-
-
-    public String getPropertyResidents(String url, String username, String password,String portfolio, String propertyId) {
-        String result = "";
-        try {
-            URL urladdress = new URL(url);
-            LeasingSoap port = AMSIUtil.getProxy(urladdress);
-            String proprequest = AMSIUtil.processHR(propertyId);
-            result = port.getPropertyResidents(username,password, portfolio, proprequest);
-            if (log.isInfoEnabled()) {
-                log.info("getPropertyResidents", result);
-            }
-        } catch (MalformedURLException e) {
-            if (log.isInfoEnabled()) {
-                log.info("MalformedURLException in getPropertyResidents", e);
-            }
-        }
-        return result;
-
-    }
-
-    public String getPropertyUnits(String url, String username, String password,String portfolio , String propertyId) {
-        String result = "";
-        try {
-            URL urladdress = new URL(url);
-            LeasingSoap port = AMSIUtil.getProxy(urladdress);
+            LeasingSoap port = AMSIUtil.getProxy();
             String proprequest = AMSIUtil.processHR(propertyId);
             result = port.getPropertyUnits(username, password, portfolio, proprequest);
             if (log.isInfoEnabled()) {
                 log.info("getPropertyUnits" , result);
             }
-        } catch (MalformedURLException e) {
-            if (log.isInfoEnabled()) {
-            	 log.info("Exception in getPropertyUnits ", e);
-            }
 
-        }
         return result;
     }
-    public String  getResidentsByStatusChangeOrTransactionDateForResidents(String url, String username, String password,String portfolio ,String propertyId) {
+    public String  getResidentsByStatusChangeOrTransactionDateForResidents( String username, String password,String portfolio ,String propertyId) {
     	String result="";
     	String result2 = "";
-    	try {
-    		URL urladdress = new URL(url);
-    		LeasingSoap port = AMSIUtil.getProxy(urladdress);
+    
+    		LeasingSoap port = AMSIUtil.getProxy();
     		String proprequestStart = AMSIUtil.processHR(propertyId,"04/20/2010",
     				"04/21/2010","", "");
     		String proprequesttran = AMSIUtil.processHR(propertyId,"",
@@ -97,32 +75,21 @@ public class AmsiService {
     			log.info("getResidentsByStatusChangeOrTransactionDateForResidents" ,  result);
     			log.info("result2",result2);
     		}
-    	} catch (MalformedURLException e) {
-    		if (log.isInfoEnabled()) {
-    			 log.info("MalformedURLException", e);
-    		}
 
-    	}
     	return result;
     }
 
-        public String  getResidentsByStatusChangeOrTransactionDateForClients(String url, String username, String password,String portfolio ,String propertyId) {
-            String result = "";
-            try {
-                URL urladdress = new URL(url);
-                LeasingSoap port = AMSIUtil.getProxy(urladdress);
+        public String  getResidentsByStatusChangeOrTransactionDateForClients(String username, String password,String portfolio ,String propertyId) {
+            String result = "";      
+
+                LeasingSoap port = AMSIUtil.getProxy();
                 String proprequest = AMSIUtil.processHR(propertyId);
                 result = port.getResidentsByStatusChangeOrTransactionDate(username, password,
                         portfolio, proprequest);
                 if (log.isInfoEnabled()) {
                     log.info("getResidentsByStatusChangeOrTransactionDateForClients" ,  result);
                 }
-            } catch (MalformedURLException e) {
-                if (log.isInfoEnabled()) {
-                	 log.info("MalformedURLException", e);
-                }
 
-            }
             return result;
         }
 
--- src/test/java/com/corelogic/rps/rentrolldata/amsi/util/AMSIUtilTest.java
+++ src/test/java/com/corelogic/rps/rentrolldata/amsi/util/AMSIUtilTest.java
@@ -4,7 +4,7 @@ package com.corelogic.rps.rentrolldata.amsi.util;
 import org.junit.Assert;
 import org.junit.Test;
 import org.w3c.dom.Document;
-import java.net.URL;
+
 
 import com.corelogic.rps.rentrolldata.amsi.generated.LeasingSoap;
 /**
@@ -15,8 +15,7 @@ public class AMSIUtilTest {
     @Test
     public void testGetProxy() throws Exception {
         boolean works=false;
-        URL url=new URL("http://www.clk-pm.net/AMSIweb/edexweb/esite/leasing.asmx");
-        LeasingSoap result = AMSIUtil.getProxy(url);
+        LeasingSoap result = AMSIUtil.getProxy();
         if (result!=null){
             works=true;
         }
@@ -33,12 +32,11 @@ public class AMSIUtilTest {
     @Test
     public void testProcessHR2() throws Exception {
         String result = AMSIUtil.processHR("62", "02/22/2015", "02/24/2015", "02/25/2015", "02/26/2015");
-        StringBuffer requeststring = new StringBuffer(261);
-        requeststring.append("<EDEX><EDEX><propertyid>").append("62")
-                .append("</propertyid><statuschangedatefrom>").append("02/22/2015")
-                .append("</statuschangedatefrom><statuschangedateto>").append("02/24/2015")
-                .append("</statuschangedateto><transactiondatefrom>").append("02/25/2015")
-                .append("</transactiondatefrom><transactiondateto>").append("02/26/2015")
+        StringBuilder requeststring = new StringBuilder(261);
+        requeststring.append("<EDEX><EDEX><propertyid>62</propertyid><statuschangedatefrom>02/22/2015")
+                .append("</statuschangedatefrom><statuschangedateto>02/24/2015")
+                .append("</statuschangedateto><transactiondatefrom>02/25/2015")
+                .append("</transactiondatefrom><transactiondateto>02/26/2015")
                 .append("</transactiondateto></EDEX></EDEX>");
 
 
